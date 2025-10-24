fbb4b015145877c1544314a607d286de47e7e07b
Sven Van Caekenberghe <sven@stfx.eu>
2025-10-24 15:12:23 +0200

    mix phx.gen.auth Accounts User users --live

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :phoenix_first, :scopes,
+  user: [
+    default: true,
+    module: PhoenixFirst.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: PhoenixFirst.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :phoenix_first,
   ecto_repos: [PhoenixFirst.Repo],
   generators: [timestamp_type: :utc_datetime]
